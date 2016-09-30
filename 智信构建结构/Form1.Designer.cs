00715a269fa910bf92253abcf525bf5e174cd60f
葛杰 <xingyu900@live.com>
2016-09-30 12:50:06 +0800

    名称

--- 智信构建结构/Form1.Designer.cs
+++ 智信构建结构/Form1.Designer.cs
@@ -101,7 +101,7 @@
             this.Controls.Add(this.lab_IP);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "郑州大河智信服务器端";
+            this.Text = "服务器端运行器";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
