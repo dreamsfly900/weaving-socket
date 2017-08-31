2574fb30c1d4b5f4906d6885d157d2fa0c77ad0d
葛杰 <xingyu900@live.com>
2017-08-31 18:50:51 +0800

    nuget打包

--- Code/TCPclient/Properties/AssemblyInfo.cs
+++ Code/TCPclient/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("TCPclient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("WeavingSocketclient")]
+[assembly: AssemblyDescription("WeavingSocket的客户端类包")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TCPclient")]
+[assembly: AssemblyCompany("QQ：20573886")]
+[assembly: AssemblyProduct("WeavingSocketclient")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
