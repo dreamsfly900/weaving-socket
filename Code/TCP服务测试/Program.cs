c7356c7f4ed13000df40ea1b80a087d618915138
葛杰 <xingyu900@live.com>
2018-07-25 11:34:13 +0800

    完善其他端的例子

--- Code/TCP服务测试/Program.cs
+++ Code/TCP服务测试/Program.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Weave.Base;
 using Weave.Server;
 
 namespace TCP服务测试
 {
     class Program
     {
-        static WeaveWebServer wudp = new WeaveWebServer();
+        static WeaveP2Server wudp = new WeaveP2Server(WeaveDataTypeEnum.Json);
         static void Main(string[] args)
         {
-            wudp.waveReceiveEvent += Wudp_waveReceiveEvent;
+            wudp.waveReceiveEvent += Wudp_waveReceiveEvent1;
+            //wudp.Send(soc, 0X01, "字符串");
             wudp.Start(8989);
             Console.ReadLine();
+        }
+
+        private static void Wudp_waveReceiveEvent1(byte command, string data, System.Net.Sockets.Socket soc)
+        {
+            
+        }
+
+        private static void Wudp_weaveReceiveBitEvent(byte command, byte[] data, System.Net.Sockets.Socket soc)
+        {
+            
         }
 
         private static void Wudp_waveReceiveEvent(byte command, string data, System.Net.Sockets.Socket soc)
